7bb0f5d9410c0e78046f821f9999facd60c15701
Prelang Builder <builder@prelang.com>
2015-10-24 15:07:33 +0000

    Defined validation for attribute 'cep' on model 'Patient'

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,5 @@ class Patient < ActiveRecord::Base
   has_many :appointments
   has_one :anamnesi
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :cep, using: :us_zip
 end
